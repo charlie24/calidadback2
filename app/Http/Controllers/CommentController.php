788bf1ffb364d2357ea130b7903cc0f3783d7dcf
Jafeth Díaz <jafeth.diazc@gmail.com>
2019-06-23 17:27:55 -0500

    ticket commented notification

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Comment;
+use App\Notifications\TicketCommented;
+use Notification;
+use Illuminate\Support\Facades\Log;
+use App\User;
 
 class CommentController extends Controller
 {
@@ -18,6 +22,19 @@ class CommentController extends Controller
         $comment->user_id = $user->id;
 
         $comment->save();
+
+        $notifiable_ids = Comment::select('user_id')
+            ->where('ticket_id', $request->ticket_id)
+            ->where('user_id', '!=', $user->id)
+            ->distinct()
+            ->pluck('user_id')
+            ->toArray();
+
+        $notifiables = User::whereIn('id', $notifiable_ids)->get();
+        $comment->user;
+        $comment->ticket;
+
+        Notification::send($notifiables, new TicketCommented($comment->toArray()));
 
         return response()->json([ 'message' => 'Successfully created'], 201);
     }
